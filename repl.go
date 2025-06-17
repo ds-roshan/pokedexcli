a44cc9b37b83fefc6744e3eb453ac771ed51197a
Roshan <ds.roshanlamichhane@gmail.com>
2025-06-17 10:46:19 +0545

    Initialize project with REPL and input cleaning logic

---
+++ repl.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if scanner.Scan() {
+			text := scanner.Text()
+			if len(text) < 1 {
+				continue
+			}
+			words := cleanInput(text)
+			fmt.Printf("Your command was: %v\n", words[0])
+		}
+	}
+}
+
+func cleanInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
+}
