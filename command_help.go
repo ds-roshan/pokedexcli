0ac154f30d44225318768a19bf75ec3050cbfed6
Roshan <ds.roshanlamichhane@gmail.com>
2025-06-17 12:24:53 +0545

    Add help and exit commands to REPL interface

---
+++ command_help.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func commandHelp() error {
+	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
+	for key, value := range getCommands() {
+		fmt.Printf("%s: %s\n", key, value.description)
+	}
+	return nil
+}
--- repl.go
+++ repl.go
@@ -7,17 +7,33 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
-		text := scanner.Text()
-		if len(text) == 0 {
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
 			continue
 		}
-		words := cleanInput(text)
-		fmt.Printf("Your command was: %v\n", words[0])
+
+		inputCommand := words[0]
+
+		command, ok := getCommands()[inputCommand]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+		if err := command.callback(); err != nil {
+			fmt.Printf("Error executing command %v with error: %v\n", inputCommand, err)
+		}
 	}
 }
 
@@ -25,4 +41,19 @@ func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 }
